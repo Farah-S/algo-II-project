714f3c8256736fc8f083dd4efbc1ef2454815078
Farah-S <farah.faroha132@gmail.com>
2022-06-01 16:21:35 +0200

    dijkstra works

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from dis import dis
 
 class Node_Distance :
 
@@ -8,48 +9,52 @@ class Node_Distance :
 
 class Graph :
 
-    def __init__ (self, node_count : int) :
+	def __init__ (self, node_count : int) :
         # Store the adjacency list as a dictionary
         # The default dictionary would create an empty list as a default (value) 
         # for the nonexistent keys.
-        self.adjlist = defaultdict(list)
-        self.node_count = node_count
+		self.adjlist = defaultdict(list)
+		self.node_count = node_count
 
-    def Add_Into_Adjlist (self, src : int, node_dist : Node_Distance(int,int)) :
-        self.adjlist[src].append(node_dist)
+	def Add_Into_Adjlist (self, src : int, node_dist : Node_Distance(int,int)) :
+		self.adjlist[src].append(node_dist)
 
-    def Dijkstras_Shortest_Path (self, source : int) :
+	def Dijkstras_Shortest_Path (self, source : int) :
 
         # Initialize the distance of all the nodes from the source node to infinity
-        distance = [999999999999] * self.node_count
+		distance = [999999999999] * self.node_count
         # Distance of source node to itself is 0
-        distance[source] = 0
+		distance[source] = 0
 
         # Create a dictionary of { node, distance_from_source }
-        dict_node_length = {source: 0}
-
-        while dict_node_length :
-
+		dict_node_length = {source: 0}
+		
+		while dict_node_length :
+			
             # Get the key for the smallest value in the dictionary
             # i.e Get the node with the shortest distance from the source
-            current_source_node = min(dict_node_length, key = lambda k: dict_node_length[k])
-            del dict_node_length[current_source_node]
-
-            for node_dist in self.adjlist[current_source_node] :
-                adjnode = node_dist.name
-                length_to_adjnode = node_dist.dist
+			current_source_node = min(dict_node_length, key = lambda k: dict_node_length[k])
+			
+			del dict_node_length[current_source_node]
+			
+			for node_dist in self.adjlist[current_source_node] :
+				adjnode = node_dist.name
+				length_to_adjnode = node_dist.dist
 
                 # Edge relaxation
-                if distance[adjnode] > distance[current_source_node] + length_to_adjnode :
-                    distance[adjnode] = distance[current_source_node] + length_to_adjnode
-                    dict_node_length[adjnode] = distance[adjnode]
+				# print(current_source_node,'----',adjnode)
+				# print(distance[current_source_node],'dc')
+				# print(distance[53],'da')
+				if distance[adjnode] > distance[current_source_node] + length_to_adjnode :
+					distance[adjnode] = distance[current_source_node] + length_to_adjnode
+					dict_node_length[adjnode] = distance[adjnode]
 
-        for i in range(self.node_count) :
-            print("Source Node ("+str(source)+")  -> Destination Node(" + str(i) + ")  : " + str(distance[i]))
+		for i in range(1,self.node_count) :
+			print("Source Node ("+str(source)+")  -> Destination Node(" + str(i) + ")  : " + str(distance[i]))
 
 def main() :
 
-	g = Graph(53)
+	g = Graph(54)
 
     # Node 1: 
 	g.Add_Into_Adjlist(1, Node_Distance(11, 2))
@@ -259,12 +264,12 @@ def main() :
 	g.Add_Into_Adjlist(52, Node_Distance(47, 7))
 
 	# Node 53:
-	g.Add_Into_Adjlist(52, Node_Distance(39, 20))
+	g.Add_Into_Adjlist(53, Node_Distance(39, 20))
 
-	g.Dijkstras_Shortest_Path(0)
+	g.Dijkstras_Shortest_Path(1)
 	print("\n")
-	g.Dijkstras_Shortest_Path(5);
+	# g.Dijkstras_Shortest_Path(53)
 
 
-if __name__ == "__main__" :
-	main()
+# if __name__ == "__main__" :
+main()
